40b430af5dc37daccce89d495b2f6d154e7c2543
Jane <jane.sullivan1@alaska.gov>
2018-04-17 16:25:55 -0800

    bring in LS model 2018 forecast results for comparison

--- HER_2018forec/her.R
+++ HER_2018forec/her.R
@@ -1,5 +1,5 @@
 
-# HER - ADFG's herring model. Original ADMB code written by SJD Martell. Helper
+# HER - ADFG's new herring model. Original ADMB code written by SJD Martell. Helper
 # files and documention contributed by M. Rudd.
 
 source("R/tools.R") 
@@ -9,12 +9,15 @@ library(R2admb)
 # clean up directory - remove unnecessary files to run
 # add ".pin" when wanting to save .pin file, or any other file to save
 setwd("HER_2018forec/")
-need_files <- c(".tpl", ".ctl", ".dat", ".R")
+need_files <- c(".tpl", ".ctl", ".dat", ".R", ".csv")
 files_present <- list.files()
 keep_index <- unlist(sapply(1:length(need_files), function(x) which(grepl(need_files[x], files_present))))
 rm_files <- files_present[-keep_index]
 remove <- sapply(1:length(rm_files), function(x) unlink(rm_files[x], TRUE))
 # setwd("..") # backs out one folder
+
+# LS model 2018
+LS_year <- read_csv("LS_2018forec_results_byyear.csv")
 
 ## compile model
 setup_admb()
@@ -23,7 +26,8 @@ compile_admb("her", verbose=TRUE)
 ## run MAP
 run_admb("her")
 
-years <- 1971:2015
+years <- 1980:2017
+ages <- 3:8
 
 #Maturity
 readMat("mat", file="her.rep", nrow = length(years))
@@ -34,13 +38,19 @@ readVec("mat_params[2]", file="her.rep")
 natmat <- readMat("Mij", file = "her.rep", nrow = length(years))[,1]
 plot(natmat ~ years, ylim=c(0, max(natmat)*1.1), type="l", lwd=2)
 
+# Selectivity
+sel <- readMat("Sij", file = "her.rep", nrow = length(years))[,1]
+plot(natmat ~ years, ylim=c(0, max(natmat)*1.1), type = "l", lwd = 2)
+
 ## read report from initial MAP run - maximum a posteriori estimation (i.e.
 ## maximum likelihood using priors!)
 ssb <- readVec("ssb", file="her.rep")
 ## put in thousands
-ssb <- ssb/1000
+# ssb <- ssb/1000
 
-plot(ssb ~ years, ylim=c(0, max(ssb)*1.1), type="l", lwd=2)
+plot(ssb ~ years, ylim = c(0, max(ssb)*1.1), type = "l", lwd = 2)
+lines(LS_year$tot_sp_B_tons ~ years, type = "l", col = "red", lwd = 2)
+
 ## run simulation with seed 123
 run_admb("her", extra.args="-sim 123")
 
