2f0b381a37f578cbb32b351c969e721ea6ecf509
Jane <jane.sullivan1@alaska.gov>
2018-06-13 12:44:19 -0800

    figure generation, helper file, and user defined fxns

--- HER_2018forec/her.R
+++ HER_2018forec/her.R
@@ -1,11 +1,250 @@
 
 # HER - ADFG's new herring model. Original ADMB code written by SJD Martell. Helper
-# files and documention contributed by M. Rudd.
+# files and documentation contributed by M. Rudd.
 
-source("R/tools.R") 
-library(R2admb)
-library(tidyverse)
-        
+source("R/tools.r")
+source("R/helper.r") 
+
+# Read results:
+
+# Read in the data from the model report, par, and cor files.
+D <- read.admb("HER_2018forec/her") # read.admb() from globals.r
+
+sb.file <- "HER_2018forec/ssb.ps" # Will only exist if you've run -mceval
+if(file.exists(sb.file)){
+  D$post.samp.ssb=read.table(sb.file)
+  colnames(D$post.samp.ssb) <- paste0("year",D$year)
+}
+
+
+plot.catch <- function(D = D, nm = "data_ct_raw",...) {
+  df <- as.data.frame(D[[nm]])
+  colnames(df) <- c("Year","Catch","log.se")
+  # z  <- 1.96
+   df <- df %>% 
+    mutate(ln.ct = log(Catch),
+           lci = exp(ln.ct - z*log.se),
+           uci = exp(ln.ct + z*log.se),
+           std = 1.96*sqrt(log(log.se+1)),
+           lower = Catch - std * Catch,
+           upper = Catch + std * Catch)
+  
+  tickr(df, Year, 5)
+  ggplot(df,aes(Year,Catch)) +
+    geom_point() +
+    geom_pointrange(aes(ymin = lci, ymax = uci),size=0.5,fatten=2) + 
+    labs(x="Year")
+}
+
+
+plot.waa <- function(D=D, nm = "data_sp_waa",...) {
+  df <- as.data.frame(D[[nm]])
+  colnames(df) <- c("Year",paste(D[['sage']]:D[['nage']]))
+  gdf <- 	gather(df,"Age","Weight.at.age",-Year) %>% 
+    transform(Age=as.integer(Age)) %>% 
+    mutate(Cohort=as.factor(Year-Age))
+  
+  ggplot(gdf,aes(Year,Weight.at.age,color=Cohort)) + 
+    geom_line(alpha=0.90) + 
+    geom_point(alpha=0.5,aes(fill=Cohort),show.legend=FALSE,size=0.5) +
+    labs(x="Year",y="Weight-at-age (grams)",color="Cohort") +
+    guides(col = guide_legend(ncol = 9)) +
+    theme(legend.position="bottom") +ggtitle(D$Model)
+}
+
+plot.comp <- function(D=D, nm = "data_cm_comp",...) {
+  df <- as.data.frame(D[[nm]])
+  df[df==-9] <- NA
+  colnames(df) <- c("Year",paste(D[['sage']]:D[['nage']]))
+  gdf <- 	gather(df,"Age","Proportion",-Year) %>%
+    transform(Age=as.integer(Age)) %>%
+    mutate(Cohort=as.factor(Year-Age))
+  
+  ggplot(gdf,aes(Year,Age,color=Cohort)) + 
+    geom_point(alpha=0.5,aes(size=abs(Proportion)),show.legend=FALSE) +
+    scale_size_area(max_size=8) + 
+    labs(x="Year",...) + ggtitle(D$Model)
+}
+
+plot.ssb <- function(D=D){
+  #qplot(D$year,D$ssb/1000,geom="line") + ylim(c(0,NA)) +
+  #labs(x="Year",y="Female Spawning Stock Biomass (1000 mt)")
+  
+  # df <- data.frame(year=seq(D$mod_syr,D$mod_nyr),ssb=D$ssb)
+  # ggplot(df,aes(year,ssb/1000)) +geom_line() + ylim(c(0,NA)) + 
+  # labs(x="Year",y="Female Spawning Stock Biomass (1000 mt)") +
+  # ggtitle(D$Model)
+  
+  
+  id <- grep("sd_ssb",D$fit$names)
+  
+  ssb.df <- data.frame(year=seq(D$mod_syr,D$mod_nyr),
+                       SSB = D$fit$est[id]/1000,
+                       sdSSB = D$fit$std[id]/1000) %>% 
+    mutate(lci=SSB-1.96*sdSSB,uci=SSB+1.96*sdSSB)
+  
+  ggplot(ssb.df,aes(year,SSB)) + 
+    geom_line() +
+    geom_ribbon(aes(x=year,ymin=lci,ymax=uci),alpha=0.15)+
+    labs(x="Year",y="Female Spawning Stock Biomass (1000 mt)") +
+    ggtitle(D$Model)
+  
+  
+}
+
+plot.resd <- function(D=D, nm = "resd_cm_comp", ...) {
+  
+  # Dealing with composition data.
+  if( grepl("comp",nm) ){
+    df <- as.data.frame(cbind(D[['mod_syr']]:D[['mod_nyr']],D[[nm]]))
+    # df[df==-9] <- NA
+    colnames(df) <- c("Year",paste(D[['sage']]:D[['nage']]))
+    gdf <- 	gather(df,"Age","Residual",-Year) %>%
+      transform(Age=as.integer(Age)) %>%
+      mutate(Cohort=as.factor(Year-Age))
+    
+    ggplot(gdf,aes(Year,Age,color=factor(sign(Residual)))) + 
+      geom_point(alpha=0.5,aes(size=abs(Residual)),show.legend=TRUE) +
+      scale_size_area(max_size=8) + 
+      labs(x="Year",color="Sign",size="Residual",...)+
+      ggtitle(D$Model)
+    
+  } else if( grepl("egg",nm) ) {
+    df <- as.data.frame(cbind(D[['year']],D[[nm]]))
+    colnames(df) <- c("Year","Residual")
+    
+    ggplot(df,aes(Year,Residual)) + geom_point() +
+      geom_segment(aes(x = Year, xend = Year, y = 0, 
+                       yend = Residual),data=df,size=0.2) +
+      labs(y="Residual (egg deposition)")+
+      ggtitle(D$Model)
+    
+  } else if( grepl("rec",nm) ) {
+    df <- as.data.frame(cbind(D[['rec_years']],D[[nm]]))
+    colnames(df) <- c("Year","Residual")
+    
+    ggplot(df,aes(Year,Residual)) + geom_point() +
+      geom_segment(aes(x = Year, xend = Year, y = 0, 
+                       yend = Residual),data=df,size=0.2)+
+      labs(y="Residual (recruitment deviation)")+
+      ggtitle(D$Model)
+  } else if( grepl("catch",nm) ) {
+    df <- as.data.frame(cbind(D[['year']],D[[nm]]))
+    colnames(df) <- c("Year","Residual")
+    
+    ggplot(df,aes(Year,Residual)) + geom_point() +
+      geom_segment(aes(x = Year, xend = Year, y = 0, 
+                       yend = Residual),data=df,size=0.2)+
+      labs(y="Residual (commercial catch)") +
+      ggtitle(D$Model)
+  }
+}
+
+plot.ft <- function(D) {
+  id <- grep("log_ft_pars",D$fit$names)
+  log.ft.mle <- D$fit$est[id]
+  log.ft.std <- D$fit$std[id]
+  
+  df <- data.frame(Year=D$year,Ft = exp(log.ft.mle),
+                   lci = exp(log.ft.mle-1.96*log.ft.std),
+                   uci = exp(log.ft.mle+1.96*log.ft.std))
+  
+  ggplot(df,aes(Year,Ft)) + geom_line() + 
+    geom_ribbon(aes(x=Year,ymin=lci,ymax=uci),alpha=0.15)+
+    labs(y="Instantaneous fishing mortality") + 
+    ggtitle(D$Model)
+  
+}
+
+plot.ft.post <- function(D=D) {
+  
+  # if(is.null(D$post.samp)) return()
+  ps <- D$post.samp
+  ix <- sample(1:ncol(ps),1000,replace=TRUE)
+  ps <- ps[ix,]
+  colnames(ps) <- D$fit$names[1:ncol(ps)]	
+  
+  # select the log_ft_pars columns
+  px <- ps[,grepl("log_ft_pars",colnames(ps))]
+  yr <- seq(D$mod_syr,D$mod_nyr)	
+  px <- as.data.frame(px)
+  colnames(px) <- paste(yr)
+  
+  # gather
+  gx <- gather(px,Year,Value)
+  # plot
+  ggplot(gx,aes(Year,exp(Value))) +
+    geom_violin(alpha=0.25,fill="red",size=0.15,
+                draw_quantiles = c(0.25, 0.5, 0.75)) +
+    labs(x="Year",y="Average fishing mortality rate (ft)")+
+    ylim(c(0,NA))+
+    scale_x_discrete(breaks=seq(D$mod_syr,D$mod_nyr,by=5))
+  
+}
+
+# Spawning stock biomass posterior samples
+plot.ssb.post <- function(D = D) {
+  ssb.ps <- D$post.samp.ssb
+  colnames(ssb.ps) <- paste(D$year)
+  df <- gather(ssb.ps, Year, SSB)
+  
+  ggplot(df, aes(Year, SSB / 1000)) + 
+    geom_violin(alpha = 0.25, fill = "steel blue", size = 0.25,
+                draw_quantiles = c(0.25, 0.5, 0.75)) +
+    ylim(c(0, NA)) + 
+    labs(x = "Year", y = "Spawning Stock Biomass (1000 t)\n") +
+    scale_x_discrete(breaks = seq(D$mod_syr, D$mod_nyr, by = 5))
+  
+  # }
+}
+
+# Egg deposition 
+plot.eggdepfit <- function(D = D, sfx = "egg_dep", fit = TRUE) {
+  data <- paste0("data_", sfx)
+  data <- as.data.frame(D[[data]])
+  colnames(data) <- c("year","index","log.se")
+  
+  pred <- paste0("pred_",sfx)
+  pred <- as.data.frame(cbind(D$year,D[[pred]]))
+  colnames(pred) <- c("year","pred")
+  
+  # From Sherri's bootstrap, have to add new values each year
+  U <- c(1.18, 1.12, 1.10, 0.95, 1.23, 1.13, 0.93, 1.67, 2.51, 0.98, 0.80, 1.259686251, 1.851147636, 1.751126689, 0.560987576, 1.508697833, 1.633749193, 1.695401525, 1.255367509, 2.387620843, 2.795968238, 2.215761696, 1.462234716, 2.289501604, 2.650921062, 6.384923885, 2.279693146, 2.872760889, 29.05686308, 3.863145759, 4.816134565, 4.222205391, 1.634805164, 4.043867944, 1.288746439, 1.332721825, 2.445122264,1.69865191)
+  L <- c(1.18, 1.12, 1.10, 0.95, 1.23, 1.13, 0.93, 1.67, 2.51, 0.98, 0.80, 0.985282715, 1.639191663, 1.382705136, 0.48874792, 1.398489625, 1.265029243, 1.286735024, 1.146877561, 1.827147032, 2.534746454, 1.882753246, 1.475607608, 1.863883108, 2.277982827, 3.540565615, 1.707320794, 2.568958439, 14.54490887, 3.237073047, 3.7630828, 3.942674884, 1.578639917, 2.996229014, 1.089460882, 1.154768444, 1.979792303, 1.357551379)
+  
+  df <- right_join(data, pred) %>% 
+    mutate(upper = index + U,
+           lower = index - L) %>%
+    tbl_df()
+  
+  ggplot(df, aes(year, index)) + 
+    geom_point(colour = "grey") +
+    geom_errorbar(aes(ymin = lower, ymax = upper), colour = "grey") + 
+    labs(x = "Year", y = "Egg Deposition (trillions)\n") +
+    if(fit) geom_line(aes(year, pred), alpha = 0.8) 
+  
+}
+
+
+# ---------------------------------------------------------------------------- #
+# PLOTS FOR DATA SECTION
+# ---------------------------------------------------------------------------- #
+# prefix with d for data
+# d1 » catch time series
+# d2 » spawn weight-at-age
+# d3 » commercial weight-at-age
+# d4 » commercial age-proportions
+# d5 » spawn sample age-proportions
+# d6 » Survey- and model-estimated egg deposition
+d1 <- plot.catch(D, nm = "data_ct_raw", y = "Catch (tons)")
+d2 <- plot.waa(D, nm = "data_sp_waa")
+d3 <- plot.waa(D, nm = "data_cm_waa")
+d4 <- plot.comp(D, nm = "data_cm_comp")
+d5 <- plot.comp(D, nm = "data_sp_comp")
+d6 <- plot.eggdepfit(D, sfx = "egg_dep", fit = TRUE)
+
+
+# Run models:
 
 # clean up directory - remove unnecessary files to run
 # add ".pin" when wanting to save .pin file, or any other file to save
@@ -41,7 +280,7 @@ plot(natmat ~ years, ylim=c(0, max(natmat)*1.1), type="l", lwd=2)
 
 # Selectivity
 sel <- readMat("Sij", file = "her.rep", nrow = length(years))[,1]
-plot(natmat ~ years, ylim=c(0, max(natmat)*1.1), type = "l", lwd = 2)
+plot(sel ~ years, ylim=c(0, max(natmat)*1.1), type = "l", lwd = 2)
 
 ## read report from initial MAP run - maximum a posteriori estimation (i.e.
 ## maximum likelihood using priors!)
---
+++ R/helper.R
@@ -0,0 +1,192 @@
+
+# Libraries, ggplot themes, and user-defined fxns.
+# Author: Jane Sullivan
+# Contact: jane.sullivan1@alaska.gov
+
+# libraries ----
+
+if(!require("mosaic"))   install.packages("mosaic") # derivedFactor, derivedVariable. masks over a lot of fxns, but generally only improves their utility
+if(!require("tidyverse"))   install.packages("tidyverse") # dplyr, ggplot, etc.
+if(!require("tidyr"))   install.packages("tidyr") 
+if(!require("lubridate"))   install.packages("lubridate") # dates functions like yday, dmy/mdy
+# if(!require("mgcv"))   install.packages("mgcv") # gams
+# if(!require("gridExtra"))   install.packages("gridExtra") # multipanneled plots
+if(!require("data.table"))   install.packages("data.table") # dcast, foverlaps
+# if(!require("ROracle"))   install.packages("ROracle") # database access through R
+# if(!require("broom"))   install.packages("broom") # tidying regression model output
+# if(!require("padr"))   install.packages("padr") # fills in missing values in a time series
+if(!require("knitr"))   install.packages("knitr") # r markdown
+# if(!require("forcats"))   install.packages("forcats") # releveling factors
+if(!require("cowplot"))   install.packages("cowplot") # plot_grid and so much else
+if(!require("R2admb"))   install.packages("R2admb") # run admb from r
+# install.packages("devtools")
+# devtools::install_github("ben-williams/FNGr")
+# library("FNGr")
+
+# ggplot themes ----
+
+windowsFonts(Times=windowsFont("Times New Roman"))
+
+theme_sleek <- function(base_size = 12, base_family = "Times") {
+  half_line <- base_size/2
+  theme_light(base_size = 12, base_family = "Times") +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.ticks.length = unit(half_line / 2.2, "pt"),
+      strip.background = element_rect(fill = NA, colour = NA),
+      strip.text.x = element_text(colour = "black"),
+      strip.text.y = element_text(colour = "black"),
+      #axis.text = element_text(colour = "grey30"),
+      #axis.title = element_text(colour = "grey30"),
+      #legend.title = element_text(colour = "grey30"),#, size = rel(0.9)
+      panel.border = element_rect(fill = NA),#, colour = "grey70", size = 1),
+      legend.key.size = unit(0.9, "lines"),
+      #legend.text = element_text(size = rel(0.7)),#, colour = "grey30"),
+      legend.key = element_rect(colour = NA, fill = NA),
+      legend.background = element_rect(colour = NA, fill = NA)#,
+      #plot.title = element_text(colour = "grey30"),#, size = rel(1)
+      #plot.subtitle = element_text(colour = "grey30")#, size = rel(.85)
+    )
+}
+
+theme_set(theme_sleek())
+
+# User-defined functions ----
+
+# tickr() - makes nice ticks and breaks on your ggplots
+
+# Depends on dplyr
+tickr <- function(
+  data, # dataframe
+  var, # column of interest
+  to # break point definition 
+){
+  
+  VAR <- enquo(var) # makes VAR a dynamic variable
+  
+  data %>% 
+    distinct(!!VAR) %>%
+    ungroup(!!VAR) %>% 
+    mutate(labels = ifelse(!!VAR %in% seq(to * round(min(!!VAR) / to), max(!!VAR), to),
+                           !!VAR, "")) %>%
+    select(breaks = UQ(VAR), labels)
+}
+
+# Functions from SJD Martell, formerly in globals.r
+
+
+
+
+
+
+read.admb <-
+  function(ifile)
+  {	
+    ret=read.fit(ifile)
+    
+    fn=paste(ifile,'.rep', sep='')
+    A=read.rep(fn)
+    A$fit=ret
+    
+    pfn=paste(ifile,'.psv',sep='')
+    if(file.exists(pfn))
+      A$post.samp=read.psv(pfn)
+    
+    return(A)
+  }
+
+read.fit <-
+  function(ifile)
+  {
+    # __Example:             
+    #	file <-("~/admb/simple")
+    #	A <- reptoRlist(file)
+    #	Note there is no extension on the file name.
+    
+    ## The following is a contribution from:
+    ## Anders Nielsen that reads the par & cor files.
+    ret<-list() 
+    parfile<-as.numeric(scan(paste(ifile,'.par', sep=''),   
+                             what='', n=16, quiet=TRUE)[c(6,11,16)]) 
+    ret$nopar<-as.integer(parfile[1]) 
+    ret$nlogl<-parfile[2] 
+    ret$maxgrad<-parfile[3] 
+    file<-paste(ifile,'.cor', sep='') 
+    lin<-readLines(file) 
+    ret$npar<-length(lin)-2 
+    ret$logDetHess<-as.numeric(strsplit(lin[1], '=')[[1]][2]) 
+    sublin<-lapply(strsplit(lin[1:ret$npar+2], ' '),function(x)x[x!='']) 
+    ret$names<-unlist(lapply(sublin,function(x)x[2])) 
+    ret$est<-as.numeric(unlist(lapply(sublin,function(x)x[3]))) 
+    ret$std<-as.numeric(unlist(lapply(sublin,function(x)x[4]))) 
+    ret$cor<-matrix(NA, ret$npar, ret$npar) 
+    corvec<-unlist(sapply(1:length(sublin), function(i)sublin[[i]][5:(4+i)])) 
+    ret$cor[upper.tri(ret$cor, diag=TRUE)]<-as.numeric(corvec) 
+    ret$cor[lower.tri(ret$cor)] <- t(ret$cor)[lower.tri(ret$cor)] 
+    ret$cov<-ret$cor*(ret$std%o%ret$std)
+    return(ret)
+  }
+
+read.rep <- 
+  function(fn)
+  {
+    # The following reads a report file
+    # Then the 'A' object contains a list structure
+    # with all the elemements in the report file.
+    # In the REPORT_SECTION of the AMDB template use 
+    # the following format to output objects:
+    #  	report<<"object \n"<<object<<endl;
+    #
+    # The part in quotations becomes the list name.
+    # Created By Steven Martell
+    options(warn=-1)  #Suppress the NA message in the coercion to double
+    
+    
+    ifile=scan(fn,what="character",flush=TRUE,blank.lines.skip=FALSE,quiet=TRUE)
+    idx=sapply(as.double(ifile),is.na)
+    vnam=ifile[idx] #list names
+    nv=length(vnam) #number of objects
+    A=list()
+    ir=0
+    for(i in 1:nv)
+    {
+      ir=match(vnam[i],ifile)
+      if(i!=nv) irr=match(vnam[i+1],ifile) else irr=length(ifile)+1 #next row
+      dum=NA
+      if(irr-ir==2) dum=as.double(scan(fn,skip=ir,nlines=1,quiet=TRUE,what=""))
+      if(irr-ir>2) dum=as.matrix(read.table(fn,skip=ir,nrow=irr-ir-1,fill=TRUE))
+      
+      if(is.numeric(dum))#Logical test to ensure dealing with numbers
+      {
+        A[[vnam[i]]]=dum
+      }
+    }
+    options(warn=0)
+    
+    return(A)
+  }
+
+read.psv <-
+  function(fn, nsamples=10000)
+  {
+    #This function reads the binary output from ADMB
+    #-mcsave command line option.
+    #fn = paste(ifile,'.psv',sep='')
+    filen <- file(fn, "rb")
+    nopar <- readBin(filen, what = integer(), n = 1)
+    mcmc <- readBin(filen, what = numeric(), n = nopar * nsamples)
+    mcmc <- matrix(mcmc, byrow = TRUE, ncol = nopar)
+    close(filen)
+    return(mcmc)
+  }
+
+# A simple function for creating transparent colors
+# Author: Nathan Stephens (hacks package)
+colr <- 
+  function(col.pal=1,a=1)
+  {
+    col.rgb<-col2rgb(col.pal)/255
+    rgb(t(col.rgb),alpha=a)
+  }
+
